42ca9201889d1aba47c0a9f7e05cfd21ebdefca9
glondhe <ganesh.londhe@advarisk.com>
2024-11-08 13:58:59 +0530

    Display given range on the console.

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ This repo contains problems I have solved till now using c and cpp.
 This file contains problem statements in each file.
 
 a9q1.c      : Calculate Area of circle
-
 a9q2.c      : Calculate Area of Reactangle
-
 a9q3.c      : Program to convert KM to meters.
 a9q4.c      : Program to convert Fahrenheit to celcius.
 19a5.c      : Program to convert sq feet to sq meter.
+
 a10q1.c     : Program to print given range.
+
---
+++ a10q1.c
@@ -0,0 +1,38 @@
+// Write a program to accept a range from user and print the given range on the console
+
+// Input    : 25 35
+// Output   : 25 26 27 28 29 30 31 32 33 34 35
+
+#include<stdio.h>
+
+void RangeDisplay(int iStart, int iEnd)
+{
+    if (iStart > iEnd)
+    {
+        printf("Invalid range.");
+    }
+    else
+    {
+        while(iStart <= iEnd)
+        {
+            printf("%d ", iStart);
+            iStart ++;
+        }
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+    
+    printf("Enter starting point\n");
+    scanf("%d", &iValue1);
+
+    printf("Enter End point\n");
+    scanf("%d", &iValue2);
+
+    RangeDisplay(iValue1, iValue2);
+
+    return 0;
+
+}
