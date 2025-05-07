138dd4a6be824e212adf2f552c9331bdd2bb8ba0
gkalidas <ganeshlondhe2020@gmail.com>
2025-05-07 23:27:37 +0530

    Program to print 5 to 1 on the screen.

---
+++ Program_3.c
@@ -0,0 +1,18 @@
+// Program to print 5 to 1 on the screen.
+#include<stdio.h>
+
+void Display()
+{
+    int i = 0;
+    i = 5;
+    while(i>0){
+        printf("%d ", i);
+        i--;
+    }
+}
+
+int main()
+{
+    Display();
+    return 0;
+}
