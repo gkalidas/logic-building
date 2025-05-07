b2f907d6903c93a1d5cb94d779ce69dd285f119b
gkalidas <ganeshlondhe2020@gmail.com>
2025-05-07 23:21:54 +0530

    Program to print 5 times "Marvellous" on the screen.

---
+++ Program_2.c
@@ -0,0 +1,17 @@
+#include<stdio.h> // Header inclusion
+
+void Display(){
+	int i = 0;
+	for(i = 1; i<= 5; i++)
+	{
+		printf("Marvellous\n");
+	}
+	
+}	
+
+int main()
+{
+	Display();
+
+	return 0;
+}
