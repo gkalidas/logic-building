76e7ea880c790e2885fe0d21e0d5283f55c241a3
gkalidas <ganeshlondhe2020@gmail.com>
2025-05-09 01:32:49 +0530

    Accept two numbers from user and display first number in second number of times.

---
+++ assignments/assignment_2/a2q4.c
@@ -0,0 +1,26 @@
+// Accept two numbers from user and display first number in second number of times.
+#include<stdio.h>
+
+void Display(int iNo, int iFrequency){
+    int i = 0;
+    for(i=1; i<=iFrequency; i++){
+        printf("%d  ", iNo);
+    }
+
+}
+
+int main()
+{
+    int iValue = 0;
+    int iCount = 0;
+
+    printf("Enter a number ");
+    scanf("%d", &iValue);
+    
+    printf("Enter frequency ");
+    scanf("%d", &iCount);
+
+    Display(iValue, iCount);
+
+    return 0;
+}
