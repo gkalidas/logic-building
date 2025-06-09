4c57cb6241f41055fd25573f3a5a342e3bf9bb0e
gkalidas <ganeshlondhe2020@gmail.com>
2025-06-09 20:18:11 +0530

    read file in batches

---
+++ c_programs/file_handling/program239.c
@@ -0,0 +1,32 @@
+// file handling
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>   // file control
+#include<unistd.h>  // Universal standard .h
+#include<string.h>
+
+int main(){
+    char Fname[20] = {'\0'};
+    int fd = 0, iRet = 0;
+    char Buffer[10] = {'\0'};
+
+    printf("Enter the file name that you wnat to open : \n");
+    scanf("%s", Fname);
+
+    fd = open(Fname, O_RDONLY);
+    // O_WRONLY, O_RDONLY, O_CREAT
+    if (fd == -1){
+        printf("Unable to open file \n");
+    }
+    else{
+        printf("File is successfully opened with fd : %d\n ", fd);
+        // TODO - Write your own strlen function in practice.
+
+        while((iRet=read(fd, Buffer, 10)) !=0){
+            printf("%s", Buffer);
+        }
+        
+    }
+
+    return 0;
+}
