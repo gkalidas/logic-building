aa4714c9a50f087d2846bb47240f262408d3c55e
gkalidas <ganeshlondhe2020@gmail.com>
2025-06-09 19:20:28 +0530

    writing data to file using strlen(Buffer)

---
+++ c_programs/file_handling/program232.c
@@ -0,0 +1,30 @@
+// file handling
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>   // file control
+#include<unistd.h>  // Universal standard .h
+#include<string.h>
+
+int main(){
+    char Fname[20] = {'\0'};
+    int fd = 0;
+    char Buffer[] = "India is my country";
+    int iRet = 0;
+
+    printf("Enter the file name that you wnat to open : \n");
+    scanf("%s", Fname);
+
+    fd = open(Fname, O_RDWR);
+    if (fd == -1){
+        printf("Unable to open file \n");
+    }
+    else{
+        printf("File is successfully opened with fd : %d\n ", fd);
+        // TODO - Write your own strlen function in practice.
+        iRet = write(fd, Buffer, strlen(Buffer));
+        printf("%d bytes got written successfully. ", iRet);
+        close(fd);
+    }
+
+    return 0;
+}
