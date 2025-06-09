92a85ac866306ed8b0d2b9557e9793848ec688d6
gkalidas <ganeshlondhe2020@gmail.com>
2025-06-09 19:40:10 +0530

    read file using read

---
+++ c_programs/file_handling/program236.c
@@ -0,0 +1,31 @@
+// file handling
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>   // file control
+#include<unistd.h>  // Universal standard .h
+#include<string.h>
+
+int main(){
+    char Fname[20] = {'\0'};
+    int fd = 0, iRet = 0;
+    char Buffer[50] = ['\0'];
+
+    printf("Enter the file name that you wnat to open : \n");
+    scanf("%s", Fname);
+
+    fd = open(Fname, O_RDONLY); // here is the change from previous file
+    // O_WRONLY, O_RDONLY, O_CREAT
+    if (fd == -1){
+        printf("Unable to open file \n");
+    }
+    else{
+        printf("File is successfully opened with fd : %d\n ", fd);
+        // TODO - Write your own strlen function in practice.
+        iRet = read(fd, Buffer, 11);
+        printf("%d bytes got read successfully. ", iRet);
+        printf("data from file is : %s", Buffer);
+        close(fd);
+    }
+
+    return 0;
+}
