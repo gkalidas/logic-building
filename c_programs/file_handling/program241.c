4b639935edc19bdec65321a9f5768c31fd1de7e9
gkalidas <ganeshlondhe2020@gmail.com>
2025-06-09 20:37:21 +0530

    how printf internally works.

---
+++ c_programs/file_handling/program241.c
@@ -0,0 +1,13 @@
+// file handling
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>   // file control
+#include<unistd.h>  // Universal standard .h
+#include<string.h>
+
+int main(){
+    write(1, "Jay Ganesh...",13);
+
+    
+    return 0;
+}
