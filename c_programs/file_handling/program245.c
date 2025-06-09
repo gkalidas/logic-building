b0b73956b48c1bed530960376ccae2517af9189b
gkalidas <ganeshlondhe2020@gmail.com>
2025-06-09 21:08:34 +0530

    display file size of given file

---
+++ c_programs/file_handling/program245.c
@@ -0,0 +1,39 @@
+// file handling
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>   // file control
+#include<unistd.h>  // Universal standard .h
+#include<string.h>
+
+// user defined macro
+#define BUFFER_SIZE 1024
+
+int main(){
+    char Fname[20] = {'\0'};
+    int fd = 0, iRet = 0, iSum = 0;
+    char Buffer[BUFFER_SIZE] = {'\0'};
+
+    printf("Enter the file name that you wnat to open : \n");
+    scanf("%s", Fname);
+
+    fd = open(Fname, O_RDONLY);
+    // O_WRONLY, O_RDONLY, O_CREAT
+    if (fd == -1){
+        printf("Unable to open file \n");
+    }
+    else{
+        printf("File is successfully opened with fd : %d\n ", fd);
+        // TODO - Write your own strlen function in practice.
+
+        while((iRet=read(fd, Buffer, BUFFER_SIZE)) !=0){
+            // printf("%s", Buffer);
+            // write(1, Buffer, iRet);
+            iSum = iSum + iRet;
+
+            // cleanging the garbage
+            // memset(Buffer,'\0', BUFFER_SIZE);
+        }
+        printf("File size is %d ", iSum);
+    }
+    return 0;
+}
