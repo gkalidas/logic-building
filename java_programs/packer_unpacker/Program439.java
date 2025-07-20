2f2d0bdd185b6cd80cb6af26750f1df8eeb6409b
gkalidas <ganeshlondhe2020@gmail.com>
2025-07-20 10:23:12 +0530

    Return value of read.

---
+++ java_programs/packer_unpacker/Program439.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+class Program439{
+
+    public static void main(String A[])
+    {
+        try{
+            Scanner sobj = new Scanner(System.in);
+            System.out.println("Enter the name of the file that you want to open.");
+            String Fname = sobj.nextLine();
+            File fobj = new File(Fname);
+            if(fobj.exists()){
+                FileInputStream fiobj = new FileInputStream(fobj);
+
+                byte Arr[] = new byte[10];
+                int iRet = fiobj.read(Arr);
+                String str = new String(Arr);
+                System.out.println("Data from file is "+ str);
+                System.out.println("Return value of read is "+ iRet);
+                
+            }
+            else{
+                System.out.println("file not present in current directory.");
+                return; // return value of main in java is void.
+            }
+        }
+        catch(IOException iobj){
+        }
+        catch(Exception eobj){
+        }
+    }
+}
