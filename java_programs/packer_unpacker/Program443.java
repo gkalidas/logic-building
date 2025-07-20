e201eb922e73efed2d800caae0cc1924f2daf866
gkalidas <ganeshlondhe2020@gmail.com>
2025-07-20 10:45:38 +0530

    read data from file but it's accepting 10 bytes at a time now with specific bytes.

---
+++ java_programs/packer_unpacker/Program443.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+class Program443{
+
+    public static void main(String A[])
+    {
+        try{
+            Scanner sobj = new Scanner(System.in);
+            System.out.println("Enter the name of the file that you want to open.");
+            String Fname = sobj.nextLine();
+            File fobj = new File(Fname);
+            if(fobj.exists()){
+                FileInputStream fiobj = new FileInputStream(fobj);
+
+                byte Arr[] = new byte[10];
+                int iRet = 0;
+                // String str = null;
+
+                System.out.println("Data from file is ");
+                while((iRet = fiobj.read(Arr)) != -1){
+                    String str = new String(Arr, 0, iRet);
+                    System.out.println(str.length());
+                }
+                System.out.println("Value of iRet at the end "+iRet);
+                
+            }
+            else{
+                System.out.println("file not present in current directory.");
+                return; // return value of main in java is void.
+            }
+        }
+        catch(IOException iobj){
+        }
+        catch(Exception eobj){
+        }
+    }
+}
