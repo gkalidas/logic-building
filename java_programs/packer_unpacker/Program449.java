8c8908349ec87810af7b5089199a9770a3a987e7
gkalidas <ganeshlondhe2020@gmail.com>
2025-07-20 12:01:24 +0530

    Check if given file/folder exists

---
+++ java_programs/packer_unpacker/Program449.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+class Program449{
+
+    public static void main(String A[])
+    {
+        try{
+            Scanner sobj = new Scanner(System.in);
+
+            System.out.println("Enter the name of directory : ");
+            String DirName = sobj.nextLine();
+
+            File fobj = new File(DirName);
+            if(fobj.exists()){
+                System.out.println("Directory is present...");
+                File Arr[] = fobj.listFiles();
+
+                System.out.println("No of files in the directory are "+ Arr.length);
+
+                int i = 0;
+
+                for(i=0; i< Arr.length; i++){
+                    System.out.print("File name is : " + Arr[i].getName());
+                    System.out.println(" File size is : " + Arr[i].length());
+                }
+            }
+        }
+        catch(Exception eobj){
+        }
+    }
+}
