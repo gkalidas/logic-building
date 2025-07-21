804842aaefa448c51c9b8d299f456428cc911c90
gkalidas <ganeshlondhe2020@gmail.com>
2025-07-21 14:32:13 +0530

    Create a new text file using python

---
+++ java_programs/packer_unpacker/Program453.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+class Program453{
+
+    public static void main(String A[])
+    {
+        try{
+
+        }
+        catch(Exception eobj){
+        }
+    }
+}
---
+++ python_programs/packer_unpacker/Program431.py
@@ -0,0 +1,12 @@
+
+
+
+
+
+def main():
+    with open("marvellous.txt", "w") as fobj:
+        fobj.close()
+
+
+if __name__ == "__main__":
+    main()
