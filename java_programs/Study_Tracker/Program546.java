94d53f3c36f8b81bafbefc8afcfc4a1f787f54b5
gkalidas <ganeshlondhe2020@gmail.com>
2025-07-29 16:40:33 +0530

    Adding @override method for easily tracing our objects

---
+++ java_programs/Study_Tracker/Program546.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+
+class StudyLog{
+    public String subject;
+    public Double duration;
+    public String description;
+
+    public StudyLog(String A, Double B, String C){
+        this.subject = A;
+        this.duration = B;
+        this.description = C;
+    }
+
+    public void Display(){
+        System.out.println();
+    }
+
+    @Override
+    public String toString(){
+        return subject +" | "+duration +" | "+description +".";
+    }
+}
+
+class Progra546{
+    public static void main(String A[]){
+        StudyLog sobj1 = new StudyLog("C", 2.5, "Pointer and Array.");
+        StudyLog sobj2 = new StudyLog("C++", 2.0, "Polymorphism.");
+        StudyLog sobj3 = new StudyLog("Java", 3.5, "Inheritance.");
+        
+        System.out.println(sobj1);
+        System.out.println(sobj2);
+        System.out.println(sobj3);
+
+    }
+}
