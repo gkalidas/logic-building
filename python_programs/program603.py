82b6e32c81b13a09098580171083bbccbc0ffe18
gkalidas <ganeshlondhe2020@gmail.com>
2025-08-02 18:57:37 +0530

    checking data types of data types

---
+++ python_programs/program603.py
@@ -0,0 +1,10 @@
+no1 = 10
+no2 = 21
+
+ans = no1 + no2
+
+
+print("Datatype of no is : ", type(no1))
+print("Datatype of no is : ", type(no2))
+print("Datatype of no is : ", type(ans))
+print(f"Addition of {no1} and {no2} is {ans}")
