d91e21604fc60c0e56d781d9bad661df1416de76
gkalidas <ganeshlondhe2020@gmail.com>
2025-08-02 18:59:34 +0530

    Take input from user

---
+++ python_programs/program604.py
@@ -0,0 +1,10 @@
+no1 = 0
+no2 = 0
+ans = 0
+
+no1 = int(input("Enter first number : "))
+no2 = int(input("Enter second number : "))
+
+ans = no1 + no2
+
+print(f"Addition of {no1} and {no2} is {ans}")
