8dbcce7a9f0ed9608e9c37b0bbfcee7f8735a3e6
gkalidas <ganeshlondhe2020@gmail.com>
2025-08-05 15:12:22 +0530

    Delete last function added to the linked list

---
+++ python_programs/program701.py
@@ -0,0 +1,99 @@
+class Node:
+    def __init__(self, no):
+        self.data = no
+        self.next = None
+
+class SinglyLL:
+    def __init__(self):
+        self.icount = 0
+        self.first = None
+    
+    def insert_first(self, no):
+        newn = Node(no)
+        if(self.first == None):
+            self.first = newn
+        else:
+            newn.next = self.first
+            self.first = newn
+        self.icount = self.icount + 1
+
+    def count(self):
+        return self.icount
+    
+    def display(self):
+        temp = self.first
+        while(temp != None):
+            print("|", temp.data, end="|-> ")
+            temp = temp.next
+        print("NULL")
+    
+    def insert_last(self, no):
+        newn = Node(no)
+        
+        if(self.first == None):
+            self.first = newn
+        else:
+            temp = self.first
+            while(temp.next != None):
+                temp = temp.next
+            temp.next = newn
+        self.icount = self.icount + 1
+    
+    def delete_first(self):
+        temp = self.first
+        if(temp.next == None):
+            del temp
+            self.first = None
+        else:
+            self.first = self.first.next
+            del temp
+        self.icount = self.icount - 1
+    
+    def delete_last(self):
+        temp = self.first
+        target = None
+        if(temp == None):
+            return
+        if(temp.next == None):
+            del temp
+            self.first = None
+        else:
+            while(temp.next.next != None):
+                temp = temp.next
+            target = temp.next.next
+            temp.next = None
+            del target
+        self.icount = self.icount - 1
+
+
+def main():
+    sobj = SinglyLL()
+    sobj.insert_first(51)
+    sobj.insert_first(21)
+    sobj.insert_first(11)
+
+    sobj.display()
+    i_ret = sobj.count()
+    print("No of elements in Linked list are ", i_ret)
+
+    sobj.insert_last(101)
+    sobj.insert_last(111)
+    sobj.insert_last(121)
+    
+    sobj.display()
+    i_ret = sobj.count()
+    print("No of elements in Linked list are ", i_ret)
+
+    sobj.delete_first()
+    sobj.display()
+    i_ret = sobj.count()
+    print("No of elements in Linked list are ", i_ret)
+
+    sobj.delete_last()
+    sobj.display()
+    i_ret = sobj.count()
+    print("No of elements in Linked list are ", i_ret)
+    
+
+if __name__ == "__main__":
+    main()
